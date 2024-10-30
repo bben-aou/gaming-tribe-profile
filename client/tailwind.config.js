2efb9e1f6ff0a2d86e7f55a4c236f20432387b6c
Bilal Ben Aouad <bilal.benaouad@yakeey.com>
2024-10-30 18:54:39 +0100

    chore : add packaages and configuration of ts and vite

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+    darkMode: ["class"],
+    content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+  	extend: {
+  		colors: {
+  			primary: {
+  				'100': '#161a1d',
+				'200' : '#4bce97',
+				"300": '#1d2125',
+				"400" : "#2AB37A"
+  			},
+			hover :{ 
+				"100" : "#60e0aafb"
+				
+			},
+			light : {
+				"100" : "#C7D1DB"
+			}
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 }
-
